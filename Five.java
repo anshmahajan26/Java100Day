2de9401537fd94d7efd116dc93456faff771f533
anshmahajan26 <anshmahajan678@gmail.com>
2025-09-21 09:17:30 +0530

    Day one

---
+++ Five.java
@@ -0,0 +1,23 @@
+public class Five
+ {
+   public static void main (String[]args)
+   {
+
+     int num1 = 50, num2 = 20;
+     if (num1 == num2)
+       System.out.println ("both are equal");
+     else if (num1 > num2)
+         System.out.println (num1 + " is greater");
+
+     else
+         System.out.println (num2 + " is greater");
+
+   }
+ }
+
+        //  int num1 = 12, num2 = 21;
+        // if (num1 == num2)
+        //     System.out.println("both are equal");
+        // else
+        //     // prints the maximum of two numbers
+        //     System.out.println(Math.max(num1, num2) + " is greater");
