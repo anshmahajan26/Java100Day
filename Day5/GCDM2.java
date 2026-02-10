0c107f7f31a5f1257375962b95e3fe1248c39c47
anshmahajan26 <anshmahajan678@gmail.com>
2026-02-10 23:04:55 +0530

    DONE

---
+++ Day5/GCDM2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class GCDM2 
+{
+    public static int gcd(int num1, int num2) 
+    {
+        if (num2 == 0)
+            return num1;
+    
+        return gcd (num2, num1 % num2);
+    }
+
+    public static void main (String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println("GCD of "+num1+" and "+num2+" is "+gcd(num1,num2));
+    }
+}
