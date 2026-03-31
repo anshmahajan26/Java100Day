baf5c00d71a85bc38b809f26f4e7153b1ba5ed79
anshmahajan26 <anshmahajan678@gmail.com>
2026-03-31 08:55:57 +0530

    done

---
+++ Day6/nqt2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class nqt2{
+    public static int Binary(String str){
+        if(str == null) return -1;
+        
+    }
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+    }
+}
