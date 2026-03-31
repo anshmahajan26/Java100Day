e8b74fec62d069325fbd75d600193d13376fa58e
anshmahajan26 <anshmahajan678@gmail.com>
2026-03-31 08:20:47 +0530

    start for day

---
+++ Day6/nqt1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class nqt1{
+
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr1[]=new int [n];
+        for(int i =0; i<n; i++){
+            arr1[i]= sc.nextInt();
+        }
+       for(int i =0; i<arr1.length; i++){
+        System.out.println(arr1[i]);
+       }
+        int n1 = sc.nextInt();
+
+
+
+
+    }
+}
