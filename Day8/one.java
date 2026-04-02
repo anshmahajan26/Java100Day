7942af22ea1f69cff33f7bc1f893d37b24b7375b
anshmahajan26 <anshmahajan678@gmail.com>
2026-04-02 09:42:50 +0530

    done

---
+++ Day8/one.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class one{
+    public static int Binary(String str){
+        if(str == null) return -1;
+        int result = str.charAt(0)- '0';
+
+        for(int i =1; i<str.length(); ){
+            char ch = str.charAt(i);
+            i++;
+            if(ch == 'A'){
+                result = result & str.charAt(i) - '0';
+            }else if(ch == 'B'){
+                result = result | str.charAt(i) - '0';
+            }else{
+                result = result ^ str.charAt(i) - '0';
+            }
+            i++;
+        }
+        return result;
+
+    }
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.print(Binary(str));
+    }
+}
