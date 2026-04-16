58fa0b2d37239dbd461471c1deb184a053160ae2
anshmahajan26 <anshmahajan678@gmail.com>
2026-04-17 00:48:15 +0530

    done

--- Day9/two.java
+++ Day9/two.java
@@ -4,7 +4,7 @@ public class two{
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println("hellow world")
+        System.out.println("hellow world");
 
     }
 }
